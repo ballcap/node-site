e07a74c50a864a59f3b2ea505278e3f4a4bab754
ballcap <aalikhan1988@gmail.com>
2024-12-14 09:38:29 +0900

    Update server.js

--- server.js
+++ server.js
@@ -23,9 +23,20 @@ app.get('/', (req, res) => {
 // API route for dynamic content
 app.get('/api/content', (req, res) => {
   const pages = {
-    home: { message: 'Homepage', message2: 'Hello World!' },
-    contact: { message: 'Get in Touch', message2: 'Contact Us' },
-    about: { message: 'About Us', message2: 'Who We Are' }
+    womansthreat: { message: 'R. Kelly', message2: 'A Womans Threat' },
+    icanfly: { message: 'R. Kelly', message2: 'I Can Fly' },
+    iwish: { message: 'R. Kelly', message2: 'I Wish' },
+    worldsgreatest: { message: 'R. Kelly', message2: 'The Worlds Greatest' },
+    allyall: { message: 'Timbaland', message2: 'All Yall' },
+    bombay: { message: 'Timbaland', message2: 'Bombay' },
+    problems: { message: 'Timbaland', message2: '99 Problems' },
+    putyourhandswhere: { message: 'Busta Rhymes', message2: 'Put Your Hands Where My Eyes Can See' },
+    breakyaneck: { message: 'Busta Rhymes', message2: 'Break Ya Neck' },
+    familybusiness: { message: 'Kanye West', message2: 'Family Business' },
+    spaceship: { message: 'Kanye West', message2: 'Spaceship' },
+    driveslow: { message: 'Kanye West', message2: 'Drive Slow' },
+    champion: { message: 'Kanye West', message2: 'Champion' },
+    bigbrother: { message: 'Kanye West', message2: 'Big Brother' }
   };
 
   const page = req.query.page || 'home';  // Default to home
